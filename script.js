ace1a0cf6d1398b1d853c4b887c54414ed7d03c1
Herman Sabo <sabo@Hermans-MacBook-Pro.local>
2021-03-20 00:29:41 +0400

    adds bg cover and paint func.

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ wrapper.appendChild(playArea);
 const gridHight = 36;
 const gridWidth = 36;
 const gridSize = gridHight * gridWidth;
+const brushColor = 'white';
+
+// HERO text
+const heroTextWrapper = document.createElement('div');
+const heroText = document.createElement('div');
+heroText.classList.add('heroText');
+heroText.innerText = 'Etch-a-scetch';
+heroTextWrapper.appendChild(heroText);
+playArea.appendChild(heroTextWrapper);
+
+
+// COLOR picker 
+const colorInput = document.createElement('input');
+colorInput.setAttribute('type', 'color');
+colorInput.setAttribute('id', 'colorInput');
+colorInput.setAttribute('name', 'colorInput');
+colorInput.setAttribute('value', 'brushColor');
+
+const inputLabel = document.createElement('label');
+inputLabel.setAttribute('for', 'colorInput');
+inputLabel.innerText = "Pick a color";
+
+
+
 
 
 function createGrid2(height, width) {
@@ -15,15 +39,20 @@ function createGrid2(height, width) {
 		for (let j = 0; j < width; j++) {
 			let gridUnit = document.createElement('div');
 			gridUnit.classList.add('gridUnit');
+			gridUnit.addEventListener("mouseover", function( event ) {
+				// highlight the mouseover target
+				event.target.style.backgroundColor = `${brushColor}`;
+			}, false);
 			let gridUnitWidth = playArea.clientWidth / gridWidth;
 			console.log(playArea.clientWidth);
 			gridUnit.style.width = gridUnitWidth + 'px';
 			gridUnit.style.height = gridUnitWidth + 'px';
-			gridUnit.innerText = j;
+			// gridUnit.innerText = j;
 			gridRow.appendChild(gridUnit);
 		}		
-		playArea.appendChild(gridRow);
-	}			
+		playArea.appendChild(gridRow);	
+	}		
+	console.log(playArea.clientWidth);	
 }
 
 createGrid2(gridHight, gridWidth);
